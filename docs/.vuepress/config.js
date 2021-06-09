46d165d1ba073da45c2af4ecd4b3be5e10c393f7
luanchaoxiang <534265102@qq.com>
2021-06-09 20:42:50 +0800

    [modify] build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,29 +5,6 @@ module.exports = {
         nav: [
             { text: 'HOME', link: '/' },
             { text: 'ALGORITHM', link: '/algorithm/' }
-        ],
-        sidebar: {
-            '/algorithm/': [
-              '',     /* /foo/ */
-              'one',  /* /foo/one.html */
-              'two'   /* /foo/two.html */
-            ],
-      
-            '/bar/': [
-              '',      /* /bar/ */
-              'three', /* /bar/three.html */
-              'four'   /* /bar/four.html */
-            ],
-      
-            // fallback
-            '/': [
-              '',        /* / */
-              'contact', /* /contact.html */
-              'about'    /* /about.html */
-            ]
-          }
-    },
-    // themeConfig: {
-    //     logo: 'assets/img/cover.jpg'
-    // }
+        ]
+    }
 }
